0b525751cacc29a50197367ac3dc57ec52c50d71
aimar62 <lijiangbing62@163.com>
2018-10-19 23:29:00 +0800

    this is my first commit

---
+++ hw/hw/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw
+//
+//  Created by Bryan.Li on 2018/10/19.
+//  Copyright © 2018年 aimarDesignPattehwrn. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
